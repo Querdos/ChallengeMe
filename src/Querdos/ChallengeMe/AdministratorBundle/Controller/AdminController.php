b4fdd675173231e3cb1b0b2603abec66ff308011
Hamza ESSAYEGH <essayegh@et.esiea.fr>
2016-05-21 21:09:01 +0200

    Installation du thème principal en coordination avec assetic

--- src/Querdos/ChallengeMe/AdministratorBundle/Controller/AdminController.php
+++ src/Querdos/ChallengeMe/AdministratorBundle/Controller/AdminController.php
@@ -5,12 +5,15 @@ namespace Querdos\ChallengeMe\AdministratorBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class AdminController extends Controller
 {
     /**
      * @Route("/", name="admin_homepage")
-     * @Template("AdminBundle::index.html.twig")
+     * @Template("AdminBundle:content:dashboard.html.twig")
+     *
+     * @Method("GET")
      */
     public function indexAction()
     {
