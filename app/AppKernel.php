521445d61c9027b213a46a2eb712c2de3adcf852
hessayegh <hamza.essayegh@altays.com>
2016-05-27 09:16:08 +0200

    MAJ du dossier

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             
             new Querdos\ChallengeMe\AdministratorBundle\AdminBundle(),
-            new Querdos\ChallengeMe\ConsoleBundle\ConsoleBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
