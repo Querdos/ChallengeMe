c732716a02face76740da0557d630863de7a8a7e
Hamza ESSAYEGH <essayegh@et.esiea.fr>
2016-06-01 00:14:24 +0200

    Added moderator provider

---
+++ src/Querdos/ChallengeMe/AdministratorBundle/Security/ModeratorProvider.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hamza ESSAYEGH
+ * Date: 5/31/16
+ * Time: 11:57 PM
+ */
+
+namespace Querdos\ChallengeMe\AdministratorBundle\Security;
+
+
+use Querdos\ChallengeMe\AdministratorBundle\Manager\ModeratorManager;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class ModeratorProvider implements UserProviderInterface
+{
+    /**
+     * @var ModeratorManager
+     */
+    private $moderatorManager;
+
+    /**
+     * ModeratorProvider constructor.
+     * 
+     * @param ModeratorManager $moderatorManager
+     */
+    public function __construct(ModeratorManager $moderatorManager)
+    {
+        $this->moderatorManager = $moderatorManager;
+    }
+
+    public function loadUserByUsername($username)
+    {
+        // TODO: Implement loadUserByUsername() method.
+    }
+
+    public function refreshUser(UserInterface $user)
+    {
+        // TODO: Implement refreshUser() method.
+    }
+
+    public function supportsClass($class)
+    {
+        // TODO: Implement supportsClass() method.
+    }
+}
