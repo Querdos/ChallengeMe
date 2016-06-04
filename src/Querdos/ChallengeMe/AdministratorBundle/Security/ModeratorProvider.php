c5c9b4f96fe7ba213856ee7a3bfd8141d7f058ff
Hamza ESSAYEGH <essayegh@et.esiea.fr>
2016-06-04 20:07:18 +0200

    Updated methods

--- src/Querdos/ChallengeMe/AdministratorBundle/Security/ModeratorProvider.php
+++ src/Querdos/ChallengeMe/AdministratorBundle/Security/ModeratorProvider.php
@@ -9,7 +9,11 @@
 namespace Querdos\ChallengeMe\AdministratorBundle\Security;
 
 
+use Querdos\ChallengeMe\AdministratorBundle\Entity\Administrator;
+use Querdos\ChallengeMe\AdministratorBundle\Entity\Moderator;
 use Querdos\ChallengeMe\AdministratorBundle\Manager\ModeratorManager;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 
@@ -33,16 +37,33 @@ class ModeratorProvider implements UserProviderInterface
     public function loadUserByUsername($username)
     {
         /** @var array $userData */
-//        $userData = $this->moderatorManager->
+        $userData = $this->moderatorManager->getModeratorData($username);
+
+        if ($userData) {
+            return new Moderator(
+                $userData['id'],
+                $userData['username'],
+                $userData['email'],
+                $userData['password']
+            );
+        }
+
+        throw new UsernameNotFoundException("$username doesn't exists");
     }
 
     public function refreshUser(UserInterface $user)
     {
-        // TODO: Implement refreshUser() method.
+        if (!$user instanceof Moderator) {
+            throw new UnsupportedUserException(
+                sprintf("Instance of %s are not supported", get_class($user))
+            );
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
     }
 
     public function supportsClass($class)
     {
-        // TODO: Implement supportsClass() method.
+        return $class === 'AdminBundle\\Entity\\Moderator';
     }
 }
