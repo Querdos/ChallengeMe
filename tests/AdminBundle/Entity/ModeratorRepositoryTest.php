ea33443674755941268d22cd825b1bc5485c9c8a
Hamza ESSAYEGH <essayegh@et.esiea.fr>
2016-06-05 20:17:47 +0200

    Updated ModeratorRepository tests

--- tests/AdminBundle/Entity/ModeratorRepositoryTest.php
+++ tests/AdminBundle/Entity/ModeratorRepositoryTest.php
@@ -8,9 +8,53 @@
 namespace Tests\AdminBundle\Entity;
 
 
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class ModeratorRepositoryTest /*extends KernelTestCase*/
+class ModeratorRepositoryTest extends KernelTestCase
 {
-    
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        self::bootKernel();
+
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    public function testCheckUsername()
+    {
+        // List of usernames to test
+        $usernames = ['admin', 'admin0', 'admin1', 'admin2'];
+
+        $usernamesExists = [
+            $this->em->getRepository('AdminBundle:Administrator')->checkUsername($usernames[0]),
+            $this->em->getRepository('AdminBundle:Administrator')->checkUsername($usernames[1]),
+            $this->em->getRepository('AdminBundle:Administrator')->checkUsername($usernames[2]),
+            $this->em->getRepository('AdminBundle:Administrator')->checkUsername($usernames[3])
+        ];
+
+
+        $this->assertNotNull($usernamesExists[0], "Problem with admin");
+        $this->assertNotNull($usernamesExists[1], "Problem with admin0");
+
+        $this->assertNull($usernamesExists[2], "Problem with admin1");
+        $this->assertNull($usernamesExists[3], "Problem with admin2");
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->em->close();
+        $this->em = null;
+    }
 }
