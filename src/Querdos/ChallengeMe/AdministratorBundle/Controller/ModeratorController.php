1e7644c64fcde688fc5ac648d730bc9791f9b22e
Hamza ESSAYEGH <essayegh@et.esiea.fr>
2016-06-05 13:27:34 +0200

    Added moderator controller

---
+++ src/Querdos/ChallengeMe/AdministratorBundle/Controller/ModeratorController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Hamza ESSAYEGH.
+ * Date: 6/5/16
+ * Time: 12:18 PM
+ */
+
+namespace Querdos\ChallengeMe\AdministratorBundle\Controller;
+
+
+use Symfony\Component\HttpKernel\Tests\Controller;
+
+class ModeratorController extends Controller
+{
+
+}
