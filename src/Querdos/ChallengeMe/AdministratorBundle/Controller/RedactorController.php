3730f22f94bb91581639fff8e7921e9b87bb56c2
Hamza ESSAYEGH <essayegh@et.esiea.fr>
2016-06-07 23:14:35 +0200

    Added controller for redactor

---
+++ src/Querdos/ChallengeMe/AdministratorBundle/Controller/RedactorController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Hamza ESSAYEGH.
+ * Date: 6/7/16
+ * Time: 11:14 PM
+ */
+
+namespace Querdos\ChallengeMe\AdministratorBundle\Controller;
+
+
+use Symfony\Component\HttpKernel\Tests\Controller;
+
+class RedactorController extends Controller
+{
+
+}
