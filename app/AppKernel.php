90f81da0af9d5fa6ed1b2830f43911f26718f19b
Hamza ESSAYEGH <essayegh@et.esiea.fr>
2016-08-13 17:43:17 +0200

    DEV - Added JMSTranslation bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,7 +1,7 @@
 <?php
 
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
 {
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
         	new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
 
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
             
             new Querdos\ChallengeMe\AdministratorBundle\AdminBundle(),
             new Querdos\ChallengeMe\LandingBundle\LandingBundle(),
