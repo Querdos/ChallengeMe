27f7ed25a84db9463b3a7a6194c858b88906e650
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2017-03-21 17:08:09 +0100

    DEV - Added Rating entity with corresponding manager and repository (with services)

---
+++ src/Querdos/ChallengeMe/ChallengesBundle/Manager/RatingManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Hamza ESSAYEGH
+ * User: Querdos
+ * Date: 3/21/17
+ * Time: 4:38 PM
+ */
+
+namespace Querdos\ChallengeMe\ChallengesBundle\Manager;
+
+
+class RatingManager extends BaseManager
+{
+    //
+}
---
+++ src/Querdos/ChallengeMe/ChallengesBundle/Repository/RatingRepository.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Hamza ESSAYEGH
+ * User: Querdos
+ * Date: 3/21/17
+ * Time: 4:30 PM
+ */
+
+namespace Querdos\ChallengeMe\ChallengesBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class RatingRepository extends EntityRepository
+{
+    //
+}
