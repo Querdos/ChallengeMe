ff312eabb8a1fdecc968f9c34f82a67fdeca3113
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2017-03-23 17:57:12 +0100

    DEV - Splited javascript scripts

---
+++ src/Querdos/ChallengeMe/PlayerBundle/Resources/public/js/player_scripts/inbox.js
@@ -0,0 +1,80 @@
+/**
+ * Created by querdos on 3/23/17.
+ */
+
+$('#compose, .compose-close').click(function(){
+    $('.compose').slideToggle();
+});
+
+function showMessage(author, content, sent) {
+    // retrieving blocks
+    var mail_header         = $("#mail_header");
+    var mail_sender_info    = $("#mail_sender_info");
+    var mail_view           = $("#mail_view");
+    var mail_footer         = $("#mail_footer");
+    var div_inbox           = $("#inbox_body");
+
+    div_inbox.fadeOut();
+}
+
+$(document).ready(function() {
+    function initToolbarBootstrapBindings() {
+        var fonts = ['Serif', 'Sans', 'Arial', 'Arial Black', 'Courier',
+                'Courier New', 'Comic Sans MS', 'Helvetica', 'Impact', 'Lucida Grande', 'Lucida Sans', 'Tahoma', 'Times',
+                'Times New Roman', 'Verdana'
+            ],
+            fontTarget = $('[title=Font]').siblings('.dropdown-menu');
+        $.each(fonts, function(idx, fontName) {
+            fontTarget.append($('<li><a data-edit="fontName ' + fontName + '" style="font-family:\'' + fontName + '\'">' + fontName + '</a></li>'));
+        });
+        $('a[title]').tooltip({
+            container: 'body'
+        });
+        $('.dropdown-menu input').click(function() {
+            return false;
+        })
+            .change(function() {
+                $(this).parent('.dropdown-menu').siblings('.dropdown-toggle').dropdown('toggle');
+            })
+            .keydown('esc', function() {
+                this.value = '';
+                $(this).change();
+            });
+
+        $('[data-role=magic-overlay]').each(function() {
+            var overlay = $(this),
+                target = $(overlay.data('target'));
+            overlay.css('opacity', 0).css('position', 'absolute').offset(target.offset()).width(target.outerWidth()).height(target.outerHeight());
+        });
+
+        if ("onwebkitspeechchange" in document.createElement("input")) {
+            var editorOffset = $('#editor').offset();
+
+            $('.voiceBtn').css('position', 'absolute').offset({
+                top: editorOffset.top,
+                left: editorOffset.left + $('#editor').innerWidth() - 35
+            });
+        } else {
+            $('.voiceBtn').hide();
+        }
+    }
+
+    function showErrorAlert(reason, detail) {
+        var msg = '';
+        if (reason === 'unsupported-file-type') {
+            msg = "Unsupported format " + detail;
+        } else {
+            console.log("error uploading file", reason, detail);
+        }
+        $('<div class="alert"> <button type="button" class="close" data-dismiss="alert">&times;</button>' +
+            '<strong>File upload error</strong> ' + msg + ' </div>').prependTo('#alerts');
+    }
+
+    initToolbarBootstrapBindings();
+
+    $('#editor').wysiwyg({
+        fileUploadError: showErrorAlert
+    });
+
+    prettyPrint();
+});
