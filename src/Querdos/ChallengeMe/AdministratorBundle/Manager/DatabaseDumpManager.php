daa9c445b2f812a8bf9e449c054a7b26dd6eb443
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2017-03-29 13:40:09 +0200

    DEV - Added backup feature

---
+++ src/Querdos/ChallengeMe/AdministratorBundle/Manager/DatabaseDumpManager.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Hamza ESSAYEGH
+ * User: querdos
+ * Date: 3/29/17
+ * Time: 11:42 AM
+ */
+
+namespace Querdos\ChallengeMe\AdministratorBundle\Manager;
+
+class DatabaseDumpManager extends BaseManager
+{
+    //
+}
---
+++ src/Querdos/ChallengeMe/AdministratorBundle/Repository/DatabaseDumpRepository.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Hamza ESSAYEGH
+ * User: querdos
+ * Date: 3/29/17
+ * Time: 11:27 AM
+ */
+
+namespace Querdos\ChallengeMe\AdministratorBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class DatabaseDumpRepository extends EntityRepository
+{
+    //
+}
