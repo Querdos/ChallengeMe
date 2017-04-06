82c7d6611bc9708c662d4baadf558513d2e6ac41
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2017-04-06 11:32:48 +0200

    DEV - Add the PasswordToken for reset entity

---
+++ src/Querdos/ChallengeMe/UserBundle/Manager/PasswordTokenResetManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Hamza ESSAYEGH
+ * User: querdos
+ * Date: 4/6/17
+ * Time: 11:30 AM
+ */
+
+namespace Querdos\ChallengeMe\UserBundle\Manager;
+
+
+class PasswordTokenResetManager extends BaseManager
+{
+    //
+}
---
+++ src/Querdos/ChallengeMe/UserBundle/Repository/PasswordTokenResetRepository.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Hamza ESSAYEGH
+ * User: querdos
+ * Date: 4/6/17
+ * Time: 11:24 AM
+ */
+
+namespace Querdos\ChallengeMe\UserBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class PasswordTokenResetRepository extends EntityRepository
+{
+    //
+}
