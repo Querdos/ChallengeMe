70f0a57fd69cb30dce1e146c747c31533c7b52a9
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2017-04-06 11:20:40 +0200

    DEV - Add the CronBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,10 +13,13 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-        		
+
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
         	new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+
+            new Cron\CronBundle\CronCronBundle(),
 
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
 
